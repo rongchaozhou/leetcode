52579dc1180a641d386664a82ad493d37c889692
DESKTOP-73TFD9Q\Xb <me@rongchaozhou.com>
2018-01-23 17:45:20 +0800

    Modify solution of LeetCode problem 127

--- 127_Word_Ladder/Solution.java
+++ 127_Word_Ladder/Solution.java
@@ -3,28 +3,27 @@ import java.util.*;
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         LinkedList<String> q = new LinkedList<>();
-        LinkedList<Integer> ans = new LinkedList<>();
         q.add(beginWord);
-        ans.add(1);
+        int ans = 1;
         while(q.size() != 0){
-            String temp = q.remove();
-            int aannss = ans.remove();
-            if(temp.equals(endWord)) return aannss;
-            for(int i = 0; i < wordList.size(); i++){
-                if(isonediffer(wordList.get(i), temp)){
-                    q.add(wordList.remove(i--));
-                    ans.add(aannss+1);
+            int t = q.size();
+            while(t-- > 0) {
+                String temp = q.remove();
+                if (temp.equals(endWord)) return ans;
+                for (int i = 0; i < wordList.size(); i++) {
+                    if (isOneDiff(wordList.get(i), temp)) {
+                        q.add(wordList.remove(i--));
+                    }
                 }
             }
+            ans++;
         }
         return 0;
     }
-    public boolean isonediffer(String a, String b){
+    public boolean isOneDiff(String a, String b){
         int counter = 0;
-        for(int i = 0; i< a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)){
-                counter++;
-            }
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) counter++;
         }
         return counter == 1;
     }
