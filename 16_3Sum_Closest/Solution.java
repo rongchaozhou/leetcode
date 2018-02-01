feef10fe3a62180ae510c0dff71d974cf8bd4443
DESKTOP-73TFD9Q\Xb <me@rongchaozhou.com>
2018-02-01 16:56:11 +0800

    Modify solution of LeetCode problem 16

--- 16_3Sum_Closest/Solution.java
+++ 16_3Sum_Closest/Solution.java
@@ -5,18 +5,16 @@ class Solution {
         Arrays.sort(nums);
         int ans = nums[0] + nums[1] + nums[2];
         for(int i = 0; i < nums.length; i++) {
-            int toSearch = target - nums[i];
             int lo = i + 1, hi = nums.length - 1;
             while(lo < hi) {
-                if(nums[lo] + nums[hi] == toSearch) {
+                if(nums[lo] + nums[hi] == target - nums[i]) {
                     return target;
                 }
-                int tempSum = nums[lo] + nums[hi];
-                ans = Math.abs(tempSum - toSearch) < Math.abs(ans - target) ? tempSum + nums[i] : ans;
-                if(tempSum > toSearch) {
+                ans = Math.abs(target - nums[lo] - nums[hi] - nums[i]) < Math.abs(ans - target) ? nums[lo] + nums[hi] + nums[i] : ans;
+                if(nums[lo] + nums[hi] > target - nums[i]) {
                     hi--;
                 }else {
-                    lo ++;
+                    lo++;
                 }
             }
         }
