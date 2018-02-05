00e6af912cf9aa100998199c77548bda0cc15ee8
DESKTOP-73TFD9Q\Xb <me@rongchaozhou.com>
2018-02-05 22:21:12 +0800

    Finish LeetCode problem 234

---
+++ 234_Palindrome_Linked_List/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast != null) {
+            slow = slow.next;
+        }
+        slow = reverseList(slow);
+        fast = head;
+        while(slow != null) {
+            if(fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
