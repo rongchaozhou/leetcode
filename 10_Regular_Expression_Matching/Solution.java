8e5b4ecceba04fe52b4f4f17d3a60a754d467341
DESKTOP-73TFD9Q\Xb <me@rongchaozhou.com>
2018-02-09 01:24:57 +0800

    Finish LeetCode problem 10

---
+++ 10_Regular_Expression_Matching/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0]  = true;
+        for(int j = 2; j < dp[0].length; j += 2) {
+            if(p.charAt(j-1) == '*') {
+                dp[0][j] = true;
+            }else {
+                break;
+            }
+        }
+        for(int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if(p.charAt(j - 2) != s.charAt(i - 1) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = (dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
