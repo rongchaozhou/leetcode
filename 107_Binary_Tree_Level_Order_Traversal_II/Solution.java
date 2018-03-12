a9d3c1137843b8861dee4c08a7b9849d89f25401
Rongchao Zhou <me@rongchaozhou.com>
2018-03-12 21:03:41 +0800

    Finish LeetCode problem 107

---
+++ 107_Binary_Tree_Level_Order_Traversal_II/Solution.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        Queue<TreeNode> level = new LinkedList<>();
+        if (root != null) level.offer(root);
+        while (!level.isEmpty()) {
+            List<Integer> list = new LinkedList<>();
+            int size = level.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = level.poll();
+                list.add(node.val);
+                if (node.left != null) level.offer(node.left);
+                if (node.right != null) level.offer(node.right);
+            }
+            res.addFirst(list);
+        }
+        return res;
+    }
+}
