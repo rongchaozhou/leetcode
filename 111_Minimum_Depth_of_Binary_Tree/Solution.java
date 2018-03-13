784d4ac64b93d6e8e44890ef30ff824a575002c1
Rongchao Zhou <me@rongchaozhou.com>
2018-03-13 13:58:36 +0800

    Finish LeetCode problem 111

---
+++ 111_Minimum_Depth_of_Binary_Tree/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.right == null) return 1 + minDepth(root.left);
+        if (root.left == null && root.right != null) return 1 + minDepth(root.right);
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
