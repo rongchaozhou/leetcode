22b45d83286682516eb974aa357b5f8875d00b3d
Rongchao Zhou <me@rongchaozhou.com>
2018-03-22 04:56:01 +0800

    Finish LeetCode problem 80

---
+++ 0080_Remove_Duplicates_from_Sorted_Array_II/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i <= 1 || nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
