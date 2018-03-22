ceb7577568c4bf50f03e09ccc3f53df292b8c060
Rongchao Zhou <me@rongchaozhou.com>
2018-03-23 05:29:41 +0800

    Finish LeetCode problem 33

---
+++ 0033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[lo] < nums[mid]) {
+                if (nums[lo] <= target && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else if (nums[lo] > nums[mid]) {
+                if (nums[mid] < target && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                lo++;
+            }
+        }
+        return -1;
+    }
+}
