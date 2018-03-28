eb517bace3b8fd64956ca30f58dc7f1f42d1530a
Rongchao Zhou <me@rongchaozhou.com>
2018-03-28 16:44:47 +0800

    Finish LeetCode problem 203

---
+++ 0203_Remove_Linked_List_Elements/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy, curr = head;
+        while (curr != null) {
+            if (curr.val == val) {
+                pre.next = curr.next;
+            } else {
+                pre = pre.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
