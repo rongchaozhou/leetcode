7a643345feca1b1658959a8cbe8c90be0774a255
Rongchao Zhou <me@rongchaozhou.com>
2018-03-28 16:45:12 +0800

    Finish LeetCode problem 204

---
+++ 0204_Count_Primes/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                cnt++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+        return cnt;
+    }
+}
