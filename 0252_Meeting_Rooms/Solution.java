be1c7c9137b604757e26032f9fa0e166ad8d9dcf
Rongchao Zhou <me@rongchaozhou.com>
2018-03-31 15:12:56 +0800

    Finish LeetCode problem 252

---
+++ 0252_Meeting_Rooms/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                if (o1.start == o2.start) {
+                    return o2.end - o1.end;
+                }
+                return o1.start - o2.start;
+            }
+        });
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
