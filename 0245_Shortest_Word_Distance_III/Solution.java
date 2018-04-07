2d76619bb58c8fd37a08e72ea652d2b1d10f5b3a
Rongchao Zhou <me@rongchaozhou.com>
2018-04-08 00:09:33 +0800

    Finish LeetCode problem 245

---
+++ 0245_Shortest_Word_Distance_III/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        int lastWord1 = -1, lastWord2 = -1;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                if (lastWord2 != -1) {
+                    res = Math.min(res, i - lastWord2);
+                }
+                lastWord1 = i;
+            }
+            if (words[i].equals(word2)) {
+                if (lastWord1 != -1 && lastWord1 != i) {
+                    res = Math.min(res, i - lastWord1);
+                }
+                lastWord2 = i;
+            }
+        }
+        return res;
+    }
+}
