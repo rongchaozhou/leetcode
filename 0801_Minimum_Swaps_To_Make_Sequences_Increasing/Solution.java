7527e267ca1fd391c53727e190e04b1a12138486
Rongchao Zhou <me@rongchaozhou.com>
2018-07-07 00:46:48 +0800

    Finish LeetCode problem 801

---
+++ 0801_Minimum_Swaps_To_Make_Sequences_Increasing/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSwap(int[] A, int[] B) {
+        int len = A.length;
+        int[][] dp = new int[len][2];
+        dp[0][1] = 1;
+        for (int i = 1; i < len; i++) {
+            if (A[i] > A[i - 1] && B[i] > B[i - 1] && A[i] > B[i - 1] && B[i] > A[i - 1]) {
+                dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]);
+                dp[i][1] = dp[i][0] + 1;
+            } else if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+                dp[i][0] = dp[i - 1][0];
+                dp[i][1] = dp[i - 1][1] + 1;
+            } else {
+                dp[i][0] = dp[i - 1][1];
+                dp[i][1] = dp[i - 1][0] + 1;
+            }
+        }
+        return Math.min(dp[len - 1][0], dp[len - 1][1]);
+    }
+}
