f31be4a1ac2a729dce2dfff601d9b43dec59b687
Rongchao Zhou <me@rongchaozhou.com>
2019-03-11 01:35:50 +0800

    Finish LeetCode problem 1

--- 0001_Two_Sum/Solution.java
+++ 0001_Two_Sum/Solution.java
@@ -1,9 +1,14 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> dic = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (dic.containsKey(target - nums[i])) return new int[]{dic.get(target - nums[i]), i};
-            dic.put(nums[i], i);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            }else {
+                map.put(nums[i] , i);
+            }
         }
         return null;
     }
